04d258a38204fb5b29bba484b359fbdee3484f85
tackchen <tackchen@tencent.com>
2022-05-06 14:18:44 +0800

    feat: feat: jsbox code

--- jsbox.code.js
+++ jsbox.code.js
@@ -1,8 +1,20 @@
-/*
- * @Author: tackchen
- * @Date: 2022-05-06 14:13:31
- * @LastEditors: tackchen
- * @LastEditTime: 2022-05-06 14:13:32
- * @FilePath: /qrcode/jsbox.code.js
- * @Description: Coding something
- */
+window.jsboxCode = {
+    lib: 'https://cdn.jsdelivr.net/npm/tc-qrcode/tc-qrcode.min.js',
+    lang: 'html',
+    code: /* html */`<img id='image' src='https://cdn.jsdelivr.net/gh/theajack/qrcode/helper/demo-qrcode.png'/>
+<button onclick="decodeQrcode()">Decode QRcode</button>
+<input id='input' value='Hello World!'/>
+<button onclick="encodeQrcode()">Encode QRcode</button>
+<script>
+    function decodeQrcode(){
+        TCQrcode.decodeFromImage(document.getElementById('image'))
+            .then(function (result) {
+                alert(result);
+            })
+    }
+    function encodeQrcode(){
+        const image = TCQrcode.encodeAsBase64(document.getElementById('input').value);
+        document.getElementById('image').setAttribute('src', image);
+    }
+</script>`
+};
